179419043af9ec19260ab7c3e0a6b07c19662c7c
Richard O'Brien <richard.obrien4@gmail.com>
2018-09-05 22:45:50 +0100

    Added Material table for event list

--- client/src/components/EventList/EventList.js
+++ client/src/components/EventList/EventList.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Event } from '../Event/Event';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 
 export class EventList extends Component {
 
@@ -17,8 +23,32 @@ export class EventList extends Component {
     }
 
     generateEventList() {
-
-        return (this.state.events.map(event => <Event event={event} />));
+        return (
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell date>Start Date</TableCell>
+                            <TableCell date>End Date</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.events.map(row => {
+                            return (
+                                <TableRow key={row.id}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell date>{row.startDate}</TableCell>
+                                    <TableCell date>{row.endDate}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
+        );
     }
 
     render() {
